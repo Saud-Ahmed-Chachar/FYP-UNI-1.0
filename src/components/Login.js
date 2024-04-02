dcf022f39d17b76295576039a642e81c219b7b34
Saud Ahmed <113105202+Saud-Ahmed-Chachar@users.noreply.github.com>
2024-04-03 02:50:13 +0500

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -27,7 +27,7 @@ const Login = () => {
           <div className="max-w-md mx-auto">
             <form onSubmit={handleLogin} className="divide-y divide-gray-200">
               <div className="text-center">
-                <h1 className="text-2xl font-semibold sm:text-3xl">Login</h1>
+                <h1 className="text-2xl font-semibold sm:text-3xl">University Login</h1>
               </div>
               <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                 <input
